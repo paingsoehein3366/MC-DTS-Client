63ce4e5f95c25c435c60229dc0213d0d963a568a
thuraitun <thuraitun64@gmail.com>
2024-05-24 15:43:50 +0630

    modified nofound

--- src/components/nofound/nofound.jsx
+++ src/components/nofound/nofound.jsx
@@ -1,16 +1,12 @@
-// import ErrorNoFound from "../../assets/nofound.jpg";
+import ErrorNoFound from "../../assets/nofound.gif";
+
 import "./nofound.css";
 const NoFound = () => {
   return (
-    // <div className="flex justify-center my-28">
-    //   <img className="w-[300px] h-[300px]" src={ErrorNoFound} alt="" />
-    // </div>
-    <>
-      <div className="error">404</div>
-      <br /><br />
-      <span className="info">File not found</span>
-      <img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" />
-    </>
+    <div className="flex justify-center my-28">
+      <img className="" src={ErrorNoFound} alt="" />
+    </div>
+   
   )
 }
 
